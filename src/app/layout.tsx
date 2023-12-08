8a1e15f41afae14e13526b59e90765631f36554a
umarxd <xqcowsavedmylife@gmail.com>
2023-12-08 23:07:14 +0300

    Revert "style: added ui library and main layout styling"
    This reverts commit a62feb8587d53225610a5d03a8349051bcaa299f.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,13 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="tr">
-      <body
-        className={cn("relative h-full font-sans antialiased", inter.className)}
-      >
-        <main className="relative flex flex-col min-h-screen">
-          <div className="flex-grow flex-1">{children}</div>
-        </main>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
